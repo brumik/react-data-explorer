05f820bae273d17bd6e3c74233587da2b875e73c
Levente Berky <levente.berky@gmail.com>
2021-04-19 15:54:19 +0200

    Added fetchMock to jest testing

--- src/__tests__/DataExplorer.test.tsx
+++ src/__tests__/DataExplorer.test.tsx
@@ -1,10 +1,79 @@
 import React from 'react'
-import { render } from '@testing-library/react';
-import ErrorState from '../components/Chart/Common/ErrorState';
+import fetchMock from 'fetch-mock-jest';
+import { act, render } from '@testing-library/react';
+import DataExplorer from '../components/DataExplorer';
+import {
+    ChartSchemaElement,
+    ChartKind,
+    ChartLegendOrientation,
+    ChartLegendPosition,
+    ChartTopLevelType,
+    functions
+} from '../index';
 
-describe('Foo', () => {
-    test('should render ErrorState', () => {
-        const { container } = render(<ErrorState />)
+const pieChartResponse = {
+    "items": [
+        { "host_count": 4655, "total_count": 578, "id": 2, "name": "organization_0" },
+        { "host_count": 96225, "total_count": 456, "id": -2, "name": "" },
+        { "host_count": 2995, "total_count": 411, "id": 4, "name": "organization_3" },
+        { "host_count": 2975, "total_count": 410, "id": 3, "name": "organization_1" },
+        { "host_count": 2975, "total_count": 410, "id": 1, "name": "organization_2" },
+        { "host_count": 70720, "id": -1, "name": "26 Others" }
+    ]
+};
+
+const pieChartSchema: ChartSchemaElement[] = [
+    {
+        id: 4000,
+        kind: ChartKind.wrapper,
+        type: ChartTopLevelType.pie,
+        parent: null,
+        props: {
+            height: 300,
+            x: '',
+            y: 'host_count'
+        },
+        api: {
+            params: {
+                group_by: 'org',
+                include_others: true,
+                attributes: ['host_count'],
+                sort_by: `total_count:desc`
+            },
+            url: 'http://piechart.url',
+            method: 'GET'
+        },
+        legend: {
+            interactive: true,
+            orientation: ChartLegendOrientation.vertical,
+            position: ChartLegendPosition.right
+        }
+    }
+];
+
+describe('Data Explorer', () => {
+    test('should render empty DataExplorer', () => {
+        const { container } = render(<DataExplorer
+            apis={[]}
+            schema={[]}
+            onSchemaChange={() => ({})}
+            functions={functions}
+        />)
         expect(container).toMatchSnapshot()
-    })
+    });
+
+    test('should render pie chart DataExplorer', async () => {
+        fetchMock.get({ url: 'http://piechart.url' }, pieChartResponse);
+        let container = null;
+        await act(async () => {
+            container = render(<DataExplorer
+                apis={[]}
+                schema={pieChartSchema}
+                onSchemaChange={() => ({})}
+                functions={functions}
+            />).container;
+        });
+
+        expect(container).toMatchSnapshot()
+    });
 });
