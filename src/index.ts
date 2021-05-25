c04ef080c77ea1f7b89a95133e4dc72f5c1946b8
Levente Berky <levente.berky@gmail.com>
2021-05-25 14:22:25 +0200

    Export ApiType from the index

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export * from './components/ChartEditor/types';
 export { default as ChartRenderer } from './components/Chart/index';
 export { default as functions } from './components/Chart/Functions/';
 export { default as ChartEditor } from './components/ChartEditor/index';
-export { ApiReturnType } from './components/Chart/Api/';
+export { ApiReturnType, ApiType } from './components/Chart/Api/';
 export default DataExplorer;
